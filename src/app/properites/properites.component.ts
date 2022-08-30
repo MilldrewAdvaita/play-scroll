40ed92efc01a17f186aa7b89bc452ce20ebd3b6c
Andrew Miller <andrew@advaitabio.com>
2022-08-30 05:18:23 -0400

    📅  | Tue Aug 30 05:18:23 EDT 2022

--- src/app/properites/properites.component.ts
+++ src/app/properites/properites.component.ts
@@ -9,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProperitesComponent implements OnInit {
   contaier = d3.select('#container');
   svg = this.contaier.append('svg');
-  noScrollRect = this.svg.append('rect').attr('width', 200).attr('height', 200);
+  noScrollRect = this.svg.append('rect').attr('width', 80).attr('height', 200);
+  scrollRect = this.svg
+    .append('rect')
+    .attr('width', 8000)
+    .attr('height', 200)
+    .attr('x', 80)
+    .attr('fill', 'navy');
+  content = scroll;
+
   constructor() {}
 
   ngOnInit(): void {}
